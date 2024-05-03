6b82705c61b64bb762a45e53c71aad3883b16f8e
abdulhakim10 <s.abdulhakim10@gmail.com>
2024-05-03 11:14:15 +0800

    type assertion practice

---
+++ module2/2.1.ts
@@ -0,0 +1,34 @@
+{
+// 
+// type assertion
+let anything: any;
+
+anything = 'hello';
+anything = true;
+(anything as string)
+
+const kgToGm = (value: string | number): string | number | undefined => {
+    if(typeof value === 'number'){
+        const convertedVAlue = value * 1000;
+        return `The coverted value is ${convertedVAlue}`;
+    } else if(typeof value === 'string'){
+        const numToSt = (parseFloat(value)) * 1000;
+        return `The coverted value is ${numToSt}`;
+    }
+}
+
+const result1 = kgToGm(1199) as number;
+const result2 = kgToGm('2111') as string;
+
+// type assertion with try catch
+type CustomError = {
+    message: string
+}
+
+try{
+
+} catch(error){
+    console.log((error as CustomError).message)
+}
+// 
+}
